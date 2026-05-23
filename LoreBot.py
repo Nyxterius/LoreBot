5e884adc560abb0fe505f6478ff1aa7a8d329d82
Nyxterius <77221598+Nyxterius@users.noreply.github.com>
2026-05-23 13:45:34 -0400

    Update LoreBot.py

--- LoreBot.py
+++ LoreBot.py
@@ -45,7 +45,7 @@ class Searcher():
         global result
         results = {}
         if game.lower() == "the elder scrolls":
-            results = DDGS().text(f"{game} UESP.net {topic}", region='us-en', safesearch='off', timelimit='y', page=1, backend="duckduckgo", max_results=1)
+            results = DDGS().text(f"{game} UESP {topic}", region='us-en', safesearch='off', timelimit='y', page=1, backend="duckduckgo", max_results=1)
         else:
             results = DDGS().text(f"{game} {topic} wiki", region='us-en', safesearch='off', timelimit='y', page=1, backend="duckduckgo", max_results=1)
         result = results[0]['href']
